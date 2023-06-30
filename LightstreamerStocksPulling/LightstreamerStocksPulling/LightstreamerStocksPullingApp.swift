84c42afa8efd3627cf86a15ca8b7206dda8655d0
brunno.castigrini <brunno.castigrini@valemobi.com.br>
2023-06-30 20:38:54 -0300

    FEAT: initial files

---
+++ LightstreamerStocksPulling/LightstreamerStocksPulling/LightstreamerStocksPullingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightstreamerStocksPullingApp.swift
+//  LightstreamerStocksPulling
+//
+//  Created by Brunno Castigrini on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LightstreamerStocksPullingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
