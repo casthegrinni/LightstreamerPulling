ed6076fb9ac8a0c3faf0ff1899ad3944a2ff4f43
brunno.castigrini <brunno.castigrini@valemobi.com.br>
2023-06-30 21:24:18 -0300

    FEAT: Connector + Constants

---
+++ LightstreamerStocksPulling/LightstreamerStocksPulling/ProjectFiles/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  LightstreamerStocksPulling
+//
+//  Created by Brunno Castigrini on 30/06/23.
+//
+
+import UIKit
+
+let PUSH_SERVER_URL = "https://push.lightstreamer.com"
+let ADAPTER_SET = "DEMO"
+let DATA_ADAPTER = "QUOTE_ADAPTER"
+
+let LIST_FIELDS = ["last_price", "stock_name"]
+
+let NOTIFICATION_CONN_STATUS = NSNotification.Name("LSConnectionStatusChanged")
+let NOTIFICATION_CONN_ENDED = NSNotification.Name("LSConnectionEnded")
