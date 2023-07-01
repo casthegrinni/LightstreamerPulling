15d43b47fd5dcb9f8b0780b314be682585d90ddb
brunno.castigrini <brunno.castigrini@valemobi.com.br>
2023-06-30 21:17:13 -0300

    FEAT: Stock

---
+++ LightstreamerStocksPulling/LightstreamerStocksPulling/ProjectFiles/Model/Stock.swift
@@ -0,0 +1,12 @@
+//
+//  Stock.swift
+//  LightstreamerStocksPulling
+//
+//  Created by Brunno Castigrini on 30/06/23.
+//
+
+import Foundation
+struct Stock {
+    let name: String
+    let lastPrice: Double
+}
---
+++ LightstreamerStocksPulling/LightstreamerStocksPulling/ProjectFiles/ViewModel/LightstreamerViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  LightstreamerViewModel.swift
+//  LightstreamerStocksPulling
+//
+//  Created by Brunno Castigrini on 30/06/23.
+//
+
+import Foundation
+import SwiftUI
+
+class LightstreamerViewModel: ObservableObject {
+    @Published var score = 0
+    func connect() {
+        
+    }
+    
+    func disconnect() {
+        
+    }
+}
